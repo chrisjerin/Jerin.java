81d4335c0964f2b45a04132570533756217a525e
lambo <cj021101@gmail.com>
2019-12-12 17:59:16 +0530

    hi

---
+++ src/Str.java
@@ -0,0 +1,22 @@
+public class Str {
+    public static void main(String[] args) {
+                String a="JERin";
+                System.out.println(a.length());
+                System.out.println(a.toLowerCase());
+
+                String b="Jerin";
+                        System.out.println(b.length());
+                        System.out.println(b.toUpperCase());
+
+                        String fname="Chris";
+                        String lname="Jerin";
+                        System.out.println(fname+" "+lname);
+                        System.out.println(fname+"\n"+lname);
+
+                        String d="Chris";
+                                System.out.println(d.indexOf("h"));
+
+
+            }
+        }
+
---
+++ src/TypeCast.java
@@ -0,0 +1,20 @@
+public class TypeCast {
+        public static void main(String [] args){
+            char a='j';
+            int i=(int)a;
+            System.out.println(i);
+            char b='e';
+            i=(int)b;
+            System.out.println(i);
+            char c='r';
+            i=(int)c;
+            System.out.println(i);
+            char d='i';
+            i=(int)d;
+            System.out.println(i);
+            char e='n';
+            i=(int)e;
+            System.out.println(i);
+        }
+    }
+
